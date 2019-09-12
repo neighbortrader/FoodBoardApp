7ca1b1634988fe27c8debb72588ca8196099404f
linde9821 <lindner197@gmail.com>
2019-09-12 18:06:18 +0200

    #27

---
+++ app/src/main/java/com/github/neighbortrader/foodboardapp/ui/offerDetail/OfferDetailModel.java
@@ -0,0 +1,4 @@
+package com.github.neighbortrader.foodboardapp.ui.offerDetail;
+
+public class OfferDetailModel {
+}
---
+++ app/src/main/java/com/github/neighbortrader/foodboardapp/ui/signIn/SignInModel.java
@@ -0,0 +1,4 @@
+package com.github.neighbortrader.foodboardapp.ui.signIn;
+
+public class SignInModel {
+}
