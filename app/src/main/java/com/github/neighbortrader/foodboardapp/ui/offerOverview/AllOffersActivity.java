3bd8b315bb28a13b665b1d17f5073c08909c8948
linde9821 <lindner197@gmail.com>
2019-09-16 13:58:33 +0200

    tyring to connect ui with activity

--- app/src/main/java/com/github/neighbortrader/foodboardapp/ui/offerOverview/AllOffersActivity.java
+++ app/src/main/java/com/github/neighbortrader/foodboardapp/ui/offerOverview/AllOffersActivity.java
@@ -1,16 +1,25 @@
 package com.github.neighbortrader.foodboardapp.ui.offerOverview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.crashlytics.android.Crashlytics;
 import com.github.neighbortrader.foodboardapp.R;
 import com.github.neighbortrader.foodboardapp.clientmodel.Offer;
+import com.github.neighbortrader.foodboardapp.ui.postOffer.PostOfferActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import io.fabric.sdk.android.Fabric;
 
 public class AllOffersActivity extends AppCompatActivity {
@@ -18,6 +27,15 @@ public class AllOffersActivity extends AppCompatActivity {
     public static String TAG = AllOffersActivity.class.getSimpleName();
 
     AllOffersController controller;
+
+    @BindView(R.id.mapButton)
+    Button mapButton;
+    @BindView(R.id.filterButton)
+    Button filterButton;
+    @BindView(R.id.offersListView)
+    ListView offersListView;
+    @BindView(R.id.createNewOfferFAB)
+    FloatingActionButton createNewOfferFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +47,18 @@ public class AllOffersActivity extends AppCompatActivity {
 
         controller = new AllOffersController(this);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.showoffers);
 
         controller.invokeOfferUpdate();
+
+        createNewOfferFloatingActionButton = findViewById(R.id.createNewOfferFAB);
+        createNewOfferFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startPostOfferIntent = new Intent(AllOffersActivity.this, PostOfferActivity.class);
+                AllOffersActivity.this.startActivity(startPostOfferIntent);
+            }
+        });
     }
 
     @Override
@@ -76,7 +103,7 @@ public class AllOffersActivity extends AppCompatActivity {
         Log.d(TAG, "updateUi()");
 
         for (Offer offer : offerArrayList) {
-
+            offersListView.add
         }
     }
 
--- app/src/main/java/com/github/neighbortrader/foodboardapp/ui/postOffer/PostOfferActivity.java
+++ app/src/main/java/com/github/neighbortrader/foodboardapp/ui/postOffer/PostOfferActivity.java
@@ -1,6 +1,23 @@
 package com.github.neighbortrader.foodboardapp.ui.postOffer;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
+import com.github.neighbortrader.foodboardapp.R;
+import com.github.neighbortrader.foodboardapp.ui.offerOverview.AllOffersController;
+
+import io.fabric.sdk.android.Fabric;
 
 public class PostOfferActivity extends Activity {
+
+    public static String TAG = PostOfferActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generateoffer);
+    }
 }
