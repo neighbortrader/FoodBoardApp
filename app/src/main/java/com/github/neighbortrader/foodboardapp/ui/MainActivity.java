43b7ff0861596a36db4090d6500cfce48bc25b6b
linde9821 <lindner197@gmail.com>
2019-10-04 12:15:51 +0200

    #67

--- app/src/main/java/com/github/neighbortrader/foodboardapp/ui/MainActivity.java
+++ app/src/main/java/com/github/neighbortrader/foodboardapp/ui/MainActivity.java
@@ -9,15 +9,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.crashlytics.android.Crashlytics;
 import com.github.neighbortrader.foodboardapp.R;
 import com.github.neighbortrader.foodboardapp.handler.requestsHandler.Urls;
+import com.github.neighbortrader.foodboardapp.handler.toastHandler.ToastHandler;
 import com.github.neighbortrader.foodboardapp.ui.createOffer.CreateOfferActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,11 +34,14 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.createNewOfferFAB)
     FloatingActionButton createNewOfferFloatingActionButton;
 
-
+    @BindView(R.id.activity_main)
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    @BindView(R.id.navigation)
+    public NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         Log.d(TAG, "onCreate");
 
         Fabric.with(this, new Crashlytics());
@@ -43,49 +50,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-
-
         createNewOfferFloatingActionButton.setOnClickListener(view -> {
             Intent startPostOfferIntent = new Intent(MainActivity.this, CreateOfferActivity.class);
             MainActivity.this.startActivity(startPostOfferIntent);
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+
+                case R.id.settings:
+                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+                    LayoutInflater inflater = this.getLayoutInflater();
+                    final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
+                    dialogBuilder.setView(dialogView);
+
+                    final EditText edt = dialogView.findViewById(R.id.edit1);
+
+                    edt.setOnClickListener(view1 -> {
+                        edt.setText(Urls.BASE_URL);
+                        edt.setSelection(7);
+                    });
+
+                    dialogBuilder.setTitle("IP-Adresse");
+                    dialogBuilder.setMessage("IP-Adresse und Port des Servers eingeben");
+                    dialogBuilder.setPositiveButton("Okay", (dialog, whichButton) -> {
+                        Urls.BASE_URL = edt.getText().toString();
+                    });
+                    dialogBuilder.setNegativeButton("Abbrechen", (dialog, whichButton) -> {
+
+                    });
+                    AlertDialog b = dialogBuilder.create();
+                    b.show();
+                    break;
+
+                default:
+                    return true;
+            }
+
+            return true;
+
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settings:
-                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-                LayoutInflater inflater = this.getLayoutInflater();
-                final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
-                dialogBuilder.setView(dialogView);
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
 
-                final EditText edt = dialogView.findViewById(R.id.edit1);
-
-                edt.setOnClickListener(view1 -> {
-                    edt.setText(Urls.BASE_URL);
-                    edt.setSelection(7);
-                });
-
-                dialogBuilder.setTitle("IP-Adresse");
-                dialogBuilder.setMessage("IP-Adresse und Port des Servers eingeben");
-                dialogBuilder.setPositiveButton("Okay", (dialog, whichButton) -> {
-                    Urls.BASE_URL = edt.getText().toString();
-                });
-                dialogBuilder.setNegativeButton("Abbrechen", (dialog, whichButton) -> {
-
-                });
-                AlertDialog b = dialogBuilder.create();
-                b.show();
-                return super.onOptionsItemSelected(item);
-        }
-
-        return true;
+        return super.onOptionsItemSelected(item);
     }
+
 }
