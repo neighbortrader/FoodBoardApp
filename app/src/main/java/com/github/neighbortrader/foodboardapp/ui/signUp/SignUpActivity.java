8b5c77c70df86bead5f017349a80a6fc644f3b55
linde9821 <lindner197@gmail.com>
2019-10-15 14:56:33 +0200

    added xml and intent for signUp

--- app/src/main/java/com/github/neighbortrader/foodboardapp/ui/signUp/SignUpActivity.java
+++ app/src/main/java/com/github/neighbortrader/foodboardapp/ui/signUp/SignUpActivity.java
@@ -1,6 +1,19 @@
 package com.github.neighbortrader.foodboardapp.ui.signUp;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.neighbortrader.foodboardapp.R;
+
+import butterknife.ButterKnife;
+
 public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+        ButterKnife.bind(this);
+    }
 }
