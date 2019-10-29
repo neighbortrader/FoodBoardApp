96b125e2454f12e308b179e07ff266fa5be61106
linde9821 <lindner197@gmail.com>
2019-10-29 09:03:52 +0100

    User onStatusChange when Token changes

--- app/src/main/java/com/github/neighbortrader/foodboardapp/handler/tokenHandler/TokenHandler.java
+++ app/src/main/java/com/github/neighbortrader/foodboardapp/handler/tokenHandler/TokenHandler.java
@@ -1,18 +1,22 @@
 package com.github.neighbortrader.foodboardapp.handler.tokenHandler;
 
 import com.auth0.android.jwt.JWT;
+import com.github.neighbortrader.foodboardapp.handler.clientmodelHandler.UserHandler;
 
 import lombok.Getter;
-import lombok.Setter;
 
 public class TokenHandler {
-
     @Getter
-    @Setter
     private static JWT jwtToken;
+
+    public static void setJwtToken(JWT jwtToken) {
+        TokenHandler.jwtToken = jwtToken;
+        UserHandler.callUserStatusUpdate();
+    }
 
     public static void removeToken() {
         jwtToken = null;
+        UserHandler.callUserStatusUpdate();
     }
 
     public static boolean hasToken() {
